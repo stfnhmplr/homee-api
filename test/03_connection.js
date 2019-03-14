65fe0efea43c98d787c059d2c9e9b515fff936a0
stfnhmplr <stfnhmplr@gmail.com>
2019-03-14 08:51:08 +0100

    reformatted with prettier

--- test/03_connection.js
+++ test/03_connection.js
@@ -1,19 +1,22 @@
-const {assert} = require('chai');
+const { assert } = require('chai');
 const Homee = require('../homee');
 const server = require('./MockHttpServer');
 const WebsocketServer = require('ws').Server;
 const url = require('url');
 
 const homee = new Homee('127.0.0.1', 'userxy', 'super-secret');
-const ws = new WebsocketServer({noServer: true});
+const ws = new WebsocketServer({ noServer: true });
 
 server.on('upgrade', (req, socket, head) => {
-    if (url.parse(req.url).path === '/connection?access_token=e663e30818201d28dd07803e57333bed4f15803a') {
+    if (
+        url.parse(req.url).path ===
+        '/connection?access_token=e663e30818201d28dd07803e57333bed4f15803a'
+    ) {
         ws.handleUpgrade(req, socket, head, () => {
             ws.emit('connection', ws, req);
             ws.on('message', () => {
                 ws.send(message);
-            })
+            });
         });
     } else {
         socket.destroy();
@@ -21,14 +24,11 @@ server.on('upgrade', (req, socket, head) => {
 });
 
 describe('03 connection', () => {
-
     describe('#connect via websocket', () => {
-
         it('can establish a websocket connection', () => {
             return homee.connect();
-        })
-
-    })
+        });
+    });
 
     // TODO: test event listeners
-})
+});
