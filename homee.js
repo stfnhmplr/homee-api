cb7fc9c9a80e49137ecd9c8f0224c386851f9aac
stfnhmplr <stfnhmplr@gmail.com>
2019-03-14 08:52:44 +0100

    reformatted with prettier

--- homee.js
+++ homee.js
@@ -6,19 +6,18 @@
 
 const WebSocket = require('ws');
 const request = require('request');
-const shajs = require('sha.js')
-const EventEmitter = require('events')
+const shajs = require('sha.js');
+const EventEmitter = require('events');
 const debug = require('debug')('homee');
-const Enums = require('./lib/enums')
+const Enums = require('./lib/enums');
 
 class Homee extends EventEmitter {
-
     /**
      *
      * @param host {string}
      * @param user {string}
      * @param password {string}
-     * @param options {object}
+     * @param cOptions {Object}
      */
     constructor(host, user, password, cOptions = {}) {
         super();
@@ -27,8 +26,8 @@ class Homee extends EventEmitter {
             device: 'homeeApi',
             reconnect: true,
             reconnectInterval: 5000,
-            maxRetries: Infinity
-        }
+            maxRetries: Infinity,
+        };
 
         // merge options
         for (let attr in cOptions) {
@@ -41,7 +40,10 @@ class Homee extends EventEmitter {
         this._user = user;
         this._password = password;
         this._device = options.device;
-        this._deviceId = options.device.replace(/([a-z])([A-Z])/g, '$1-$2').replace(/\s+/g, '-').toLowerCase();
+        this._deviceId = options.device
+            .replace(/([a-z])([A-Z])/g, '$1-$2')
+            .replace(/\s+/g, '-')
+            .toLowerCase();
         this._reconnectInterval = options.reconnectInterval;
         this._shouldReconnect = options.reconnect;
         this._maxRetries = options.maxRetries;
@@ -65,39 +67,46 @@ class Homee extends EventEmitter {
      * @private
      */
     _getAccessToken() {
-        debug('get access token')
+        debug('get access token');
         const options = {
             url: this._url() + '/access_token',
-            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
             form: {
                 device_name: this._device,
                 device_hardware_id: this._deviceId,
                 device_os: this.enums.CADeviceOS.CADeviceOSLinux,
                 device_type: this.enums.CADeviceType.CADeviceTypeNone,
-                device_app: this.enums.CADeviceApp.CADeviceAppHomee
+                device_app: this.enums.CADeviceApp.CADeviceAppHomee,
             },
             auth: {
                 user: this._user,
-                pass: shajs('sha512').update(this._password).digest('hex')
-            }
+                pass: shajs('sha512')
+                    .update(this._password)
+                    .digest('hex'),
+            },
         };
 
         return new Promise((resolve, reject) => {
             if (this._token && this._expires > Date.now()) {
-                debug('token still valid')
+                debug('token still valid');
                 resolve(this._token);
             }
             request.post(options, (err, res, body) => {
                 if (!err && res.statusCode === 200) {
                     this._token = body.split('&')[0].split('=')[1];
-                    this._expires = Date.now() + parseInt(body.split('&')[3].split('=')[1])*1000;
-                    debug('received access token, valid until: %s', new Date(this._expires).toISOString())
+                    this._expires = Date.now() + parseInt(body.split('&')[3].split('=')[1]) * 1000;
+                    debug(
+                        'received access token, valid until: %s',
+                        new Date(this._expires).toISOString()
+                    );
                     resolve(this._token);
                 } else if (!err && res.statusCode !== 200) {
-                    debug('cannot receive access token, received status %d', res.statusCode)
-                    reject(new Error(`cannot receive access token, received status ${res.statusCode}`));
+                    debug('cannot receive access token, received status %d', res.statusCode);
+                    reject(
+                        new Error(`cannot receive access token, received status ${res.statusCode}`)
+                    );
                 } else {
-                    debug('cannot receive access token, error %s', err)
+                    debug('cannot receive access token, error %s', err);
                     reject(new Error(`cannot receive access token, error ${err}`));
                 }
             });
@@ -113,10 +122,10 @@ class Homee extends EventEmitter {
 
         return new Promise((resolve, reject) => {
             this._getAccessToken()
-                .then((token) => {
+                .then(token => {
                     this._openWs(resolve, reject);
                 })
-                .catch((err) => {
+                .catch(err => {
                     reject(err);
                 });
         });
@@ -135,20 +144,27 @@ class Homee extends EventEmitter {
         }
 
         if (this._retries++ > this._maxRetries) {
-            debug('reached max retries %d', this._maxRetries)
+            debug('reached max retries %d', this._maxRetries);
             this.emit('maxRetries', this._maxRetries);
             return;
         }
 
         try {
-            debug('trying to connect')
-            this._ws = new WebSocket(this._wsUrl() + '/connection?access_token=' + this._token, [], {
-                protocol: 'v2', protocolVersion: 13, origin: this._url(), handshakeTimeout: 5000
-            });
+            debug('trying to connect');
+            this._ws = new WebSocket(
+                this._wsUrl() + '/connection?access_token=' + this._token,
+                [],
+                {
+                    protocol: 'v2',
+                    protocolVersion: 13,
+                    origin: this._url(),
+                    handshakeTimeout: 5000,
+                }
+            );
         } catch (err) {
-            debug('cannot open ws connection err: %s', err)
+            debug('cannot open ws connection err: %s', err);
             if (typeof reject === 'function') reject(new Error('cannot connect to homee' + err));
-            setTimeout(() => this._openWs(), this._reconnectInterval * this._retries)
+            setTimeout(() => this._openWs(), this._reconnectInterval * this._retries);
         }
 
         this._ws.on('open', () => {
@@ -163,11 +179,11 @@ class Homee extends EventEmitter {
             this.send('GET:all');
         });
 
-        this._ws.on('message', (message) => {
+        this._ws.on('message', message => {
             this._handleMessage(message);
         });
 
-        this._ws.on('close', (reason) => {
+        this._ws.on('close', reason => {
             if (!this._shouldClose && this._retries <= 1) debug('lost connection to homee');
             this._stopHeartbeathandler();
 
@@ -176,11 +192,11 @@ class Homee extends EventEmitter {
 
             this.emit('disconnected', reason);
             if (this._shouldReconnect && !this._shouldClose)
-                setTimeout(() => this._openWs(), this._reconnectInterval * this._retries)
+                setTimeout(() => this._openWs(), this._reconnectInterval * this._retries);
         });
 
-        this._ws.on('error', (err) => {
-            debug('Websocket %s', err)
+        this._ws.on('error', err => {
+            debug('Websocket %s', err);
             this.emit('error', err.toString());
         });
     }
@@ -190,11 +206,11 @@ class Homee extends EventEmitter {
      * @param {String}  message  the message, i.e. 'GET:nodes'
      */
     send(message) {
-        debug('sending message "%s" to homee', message)
+        debug('sending message "%s" to homee', message);
 
-        this._ws.send(message, (err) => {
+        this._ws.send(message, err => {
             if (err) {
-                debug('error sending message: %s', err)
+                debug('error sending message: %s', err);
                 this.emit('error', 'message could not be sent' + err);
             }
         });
@@ -225,10 +241,10 @@ class Homee extends EventEmitter {
                 this._relationships = message.all.relationships;
                 break;
             case 'attribute':
-                this._handleAttributeChange(message.attribute)
+                this._handleAttributeChange(message.attribute);
                 break;
             case 'groups':
-                this._groups = message.groups
+                this._groups = message.groups;
                 break;
             case 'node':
                 this._updateNodes(message.node);
@@ -237,7 +253,7 @@ class Homee extends EventEmitter {
                 this._nodes = message.nodes;
                 break;
             case 'relationships':
-                this._relationships = message.relationships
+                this._relationships = message.relationships;
                 break;
             case 'attribute_history':
             case 'homeegram_history':
@@ -247,7 +263,7 @@ class Homee extends EventEmitter {
 
         // broadcast on specific channel
         const ignore = ['attribute', 'attribute_history', 'homeegram_history', 'node_history'];
-        if (ignore.indexOf(message_type) === - 1) this.emit(message_type, message[message_type])
+        if (ignore.indexOf(message_type) === -1) this.emit(message_type, message[message_type]);
 
         // broadcast message
         this.emit('message', message);
@@ -259,14 +275,16 @@ class Homee extends EventEmitter {
      * @private
      */
     _handleAttributeChange(attribute) {
-        debug('attribute changed, attribute id %d', attribute.id)
+        debug('attribute changed, attribute id %d', attribute.id);
 
         if (this._nodes.length) {
-            const nodeIndex = this._nodes.findIndex(node => node.id === attribute.node_id)
-            const attributeIndex = this._nodes[nodeIndex].attributes.findIndex(a => a.id === attribute.id)
+            const nodeIndex = this._nodes.findIndex(node => node.id === attribute.node_id);
+            const attributeIndex = this._nodes[nodeIndex].attributes.findIndex(
+                a => a.id === attribute.id
+            );
 
-            this._nodes[nodeIndex].attributes[attributeIndex] = attribute
-            attribute.node = this._nodes[nodeIndex]
+            this._nodes[nodeIndex].attributes[attributeIndex] = attribute;
+            attribute.node = this._nodes[nodeIndex];
         }
 
         this.emit('attribute', attribute);
@@ -280,7 +298,12 @@ class Homee extends EventEmitter {
      * @param value {number}
      */
     setValue(device_id, attribute_id, value) {
-        debug('trying to set %d as target_value for attribute #%d (device #%d)', value, attribute_id, device_id);
+        debug(
+            'trying to set %d as target_value for attribute #%d (device #%d)',
+            value,
+            attribute_id,
+            device_id
+        );
 
         if (typeof device_id !== 'number') {
             this.emit('error', 'device_id must be a number');
@@ -306,35 +329,35 @@ class Homee extends EventEmitter {
      * @private
      */
     _startHearbeatHandler() {
-        debug('starting HearbeatHandler')
+        debug('starting HearbeatHandler');
 
         this._ws.on('pong', () => {
-            debug('received pong')
+            debug('received pong');
             this._connected = true;
         });
 
         return setInterval(() => {
-            debug('send ping')
+            debug('send ping');
             if (this._ws && this._connected === false) {
-                debug('did not receive pong, terminating connection...')
+                debug('did not receive pong, terminating connection...');
                 this._ws.terminate();
                 this._ws = null;
-                debug('lost ping, try reconnect in %ds', this._reconnectInterval / 1000)
+                debug('lost ping, try reconnect in %ds', this._reconnectInterval / 1000);
                 return;
             }
             this._connected = false;
-            this._ws.ping((err) => {
-                if (err) debug('error sending ping command to homee: %s', err.toString()) });
+            this._ws.ping(err => {
+                if (err) debug('error sending ping command to homee: %s', err.toString());
+            });
         }, 30000);
     }
-
 
     /**
      * get groups
      * @returns {Array}
      */
     get groups() {
-        return this._groups
+        return this._groups;
     }
 
     /**
@@ -361,19 +384,18 @@ class Homee extends EventEmitter {
      * @returns {Array}
      */
     getNodesByGroup(group) {
-        if (!this._relationships) throw new Error('No relationships available')
+        if (!this._relationships) throw new Error('No relationships available');
 
-        if (typeof(group) === 'string') {
-            group = this._groups.find(g => g.name === encodeURIComponent(group)).id
+        if (typeof group === 'string') {
+            group = this._groups.find(g => g.name === encodeURIComponent(group)).id;
         }
 
-        const nodeIds = this._relationships.filter(r => r.group_id === group).map(r => r.node_id)
-        return this._nodes.filter(n => nodeIds.indexOf(n.id) > -1)
+        const nodeIds = this._relationships.filter(r => r.group_id === group).map(r => r.node_id);
+        return this._nodes.filter(n => nodeIds.indexOf(n.id) > -1);
     }
 
-
     _updateNodes(node) {
-        const nodeIndex = this._nodes.findIndex(n => n.id === node.id)
+        const nodeIndex = this._nodes.findIndex(n => n.id === node.id);
         this._nodes[nodeIndex] = node;
     }
 
@@ -384,9 +406,8 @@ class Homee extends EventEmitter {
      * @param String image
      */
     createGroup(name, image = 'default') {
-        this.send(`POST:groups?name=${name}&image=${image}`)
+        this.send(`POST:groups?name=${name}&image=${image}`);
     }
-
 
     /**
      * delete a group
@@ -396,11 +417,10 @@ class Homee extends EventEmitter {
         this.send(`DELETE:groups/${id}`);
     }
 
-
     /**
      * plays a homeegram
      *
-    * @param id {number} Homeegram ID
+     * @param id {number} Homeegram ID
      */
     play(id) {
         debug('play homeegram #%d', id);
@@ -413,7 +433,7 @@ class Homee extends EventEmitter {
      */
     activateHomeegram(id) {
         debug('activate homeegram #%d', id);
-        this.send(`PUT:homeegrams/${id}?active=1`)
+        this.send(`PUT:homeegrams/${id}?active=1`);
     }
 
     /**
@@ -422,7 +442,7 @@ class Homee extends EventEmitter {
      */
     deactivateHomeegram(id) {
         debug('deactivate homeegram #%d', id);
-        this.send(`PUT:homeegrams/${id}?active=0`)
+        this.send(`PUT:homeegrams/${id}?active=0`);
     }
 
     /**
@@ -445,7 +465,7 @@ class Homee extends EventEmitter {
 
         this._ws.close(1000, 'closed by user request');
         debug('connection closed');
-        this.emit('disconnected', 'closed by user request')
+        this.emit('disconnected', 'closed by user request');
     }
 
     /**
@@ -460,8 +480,8 @@ class Homee extends EventEmitter {
         debug('request history for %s #%d', type, id);
 
         let params = '';
-        if (from) params += `from=${Math.floor(from/1000)}&`;
-        if (till) params += `till=${Math.floor(till/1000)}&`;
+        if (from) params += `from=${Math.floor(from / 1000)}&`;
+        if (till) params += `till=${Math.floor(till / 1000)}&`;
         if (limit) params += `limit=${limit}&`;
 
         switch (type) {
@@ -470,15 +490,19 @@ class Homee extends EventEmitter {
                 this.send(`GET:${type}s/${id}/history?${params}`);
                 break;
             case 'attribute':
-                const attribute = [].concat.apply([], this._nodes.map(n => n.attributes)).find(a => a.id === id);
+                const attribute = [].concat
+                    .apply([], this._nodes.map(n => n.attributes))
+                    .find(a => a.id === id);
                 this.send(`GET:nodes/${id}/attributes/${attribute.id}/history?${params}`);
                 break;
             default:
-                this.emit('error', 'history is only available for type "node", "attribute" and "homeegram"');
+                this.emit(
+                    'error',
+                    'history is only available for type "node", "attribute" and "homeegram"'
+                );
                 return;
         }
     }
-
 
     /**
      * retrieve diary entries
@@ -488,14 +512,14 @@ class Homee extends EventEmitter {
      * @param limit  Number
      */
     getDiary(from = null, till = null, limit = null) {
-        debug('request diary entries')
+        debug('request diary entries');
 
         let params = '';
-        if (from) params += `from=${Math.floor(from/1000)}&`;
-        if (till) params += `till=${Math.floor(till/1000)}&`;
+        if (from) params += `from=${Math.floor(from / 1000)}&`;
+        if (till) params += `till=${Math.floor(till / 1000)}&`;
         if (limit) params += `limit=${limit}&`;
 
-        this.send(`GET:diary?${params}`)
+        this.send(`GET:diary?${params}`);
     }
 
     /**
